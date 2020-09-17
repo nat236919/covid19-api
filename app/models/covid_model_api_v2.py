f0c2bbbb09f542eae5404bae664fcfa2dc214f3e
nat236919 <nuttaphat.aruno@gmail.com>
2020-09-17 10:24:40 +0800

    fix search country method (#58)

--- app/models/covid_model_api_v2.py
+++ app/models/covid_model_api_v2.py
@@ -86,13 +86,16 @@ class CovidAPIv2:
         """ Get a country data from its name or ISO 2 """
         all_country_data = self.get_current()['data']
 
-        # Search for a given country
-        ## In case a country name is not found, assume it's a country code (iso2) 
-        if country_name not in [country_data['location'].lower() for country_data in all_country_data]:
-            country_name = self.lookup_table[country_name.upper()] # translate country code to its name
+        # Check input
+        if not isinstance(country_name, str) or not country_name.isalpha():
+            return {}
 
-        data = [country_data for country_data in all_country_data if country_name.lower() == country_data['location'].lower()]
-        data = data[0] if data else data
+        # Search for a given country
+        country_name = country_name.lower()
+        country_name_from_code = self.lookup_table.get(country_name.upper(), '').lower()
+
+        data = [country_data for country_data in all_country_data if country_data['location'].lower() in [country_name, country_name_from_code]]
+        data = data[0] if data else {}
 
         return data
 
