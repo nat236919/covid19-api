28b53476a9a3dba2c3df6f42895939d80545cf42
Maria Sitkovets <m.sitkovets@gmail.com>
2021-03-15 09:35:29 -0400

    Use the new class within the API integrator

--- app/routers/v2/router_api_v2.py
+++ app/routers/v2/router_api_v2.py
@@ -14,9 +14,11 @@ from integrators.covid_api_v2_integrator import CovidAPIv2Integrator
 from starlette.requests import Request
 
 from . import v2
+from utils.get_data import DailyReports
 
 # Initiate Integrator
-COVID_API_V2 = CovidAPIv2Integrator()
+DAILY_REPORTS = DailyReports()
+COVID_API_V2 = CovidAPIv2Integrator(DAILY_REPORTS)
 
 
 # Logging
