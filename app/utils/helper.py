fdb1be3b0cc9e53ebd1be45d848890a09beed379
Piuli <waseefa1@yahoo.com>
2021-03-17 00:06:18 -0400

    Add new class as an aggregate root

--- app/utils/helper.py
+++ app/utils/helper.py
@@ -12,24 +12,26 @@ import pandas as pd
 import pycountry
 import requests
 
+class DataPreprocessing:
+    # Aggregate root
+    def __init__(self) -> None:
 
-# Data preprocessing (DataFrame)
-def helper_df_cleaning(df: pd.DataFrame) -> pd.DataFrame:
-    """ Fill all empty values with am empty string (e.g., '') """
-    return df.fillna('')
+    # Data preprocessing (DataFrame)
+    def helper_df_cleaning(df: pd.DataFrame) -> pd.DataFrame:
+        """ Fill all empty values with am empty string (e.g., '') """
+        return df.fillna('')
 
+    var_types = TypeVar('ensure_dtype', int, float, str)
+    # Data preprocessing (DataFrame's columns)
+    def helper_df_cols_cleaning(df: pd.DataFrame, cols: List[str], ensure_dtype: var_types = None) -> pd.DataFrame:
+        """ Clean certain columns in a DataFrame """
+        df[cols] = df[cols].fillna(0) # Replace empty cells with 0
+        df[cols] = df[cols].replace('', 0) # Replace '' with 0
 
-# Data preprocessing (DataFrame's columns)
-var_types = TypeVar('ensure_dtype', int, float, str)
-def helper_df_cols_cleaning(df: pd.DataFrame, cols: List[str], ensure_dtype: var_types = None) -> pd.DataFrame:
-    """ Clean certain colomns in a DataFrame """
-    df[cols] = df[cols].fillna(0) # Replace empty cells with 0
-    df[cols] = df[cols].replace('', 0) # Replace '' with 0
+        if ensure_dtype and ensure_dtype in [int, float, str]:
+            df[cols] = df[cols].astype(ensure_dtype)
 
-    if ensure_dtype and ensure_dtype in [int, float, str]:
-        df[cols] = df[cols].astype(ensure_dtype)
-
-    return df
+        return df
 
 
 # Get latest data
