55111bdbb57e25b8b0554876a85170537099603f
Piuli <waseefa1@yahoo.com>
2021-03-17 19:44:58 -0400

    Add new class as an aggregate root

--- app/utils/get_data.py
+++ app/utils/get_data.py
@@ -43,21 +43,42 @@ def get_data_daily_reports() -> pd.DataFrame:
     
     return df
 
+class USAData:
+    # Aggregate root
+    def __init__(self) -> None:
 
-# Get data from daily reports (USA)
-def get_data_daily_reports_us() -> pd.DataFrame:
-    """ Get data from BASE_URL_DAILY_REPORTS """
-    # Check the latest file
-    latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS_US'])
+    # Get data from daily reports (USA)
+    def get_data_daily_reports_us(self) -> pd.DataFrame:
+        """ Get data from BASE_URL_DAILY_REPORTS """
+        # Check the latest file
+        latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS_US'])
 
-    # Extract the data
-    df = pd.read_csv(latest_base_url)
+        # Extract the data
+        df = pd.read_csv(latest_base_url)
 
-    # Data pre-processing
-    concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active']
-    df = helper_df_cols_cleaning(df, concerned_columns, int)
-    
-    return df
+        # Data pre-processing
+        concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active']
+        df = helper_df_cols_cleaning(df, concerned_columns, int)
+
+        return df
+
+    # Get data from time series (US)
+    def get_US_time_series(self) -> Dict[str, pd.DataFrame]:
+        """ Get the dataset of time series for USA """
+        dataframes = {}
+
+        # Iterate through categories ('confirmed', 'deaths')
+        for category in JHU_CSSE_FILE_PATHS['CATEGORIES'][:-1]:
+            url = JHU_CSSE_FILE_PATHS['BASE_URL_US_TIME_SERIES'].format(category)
+
+            # Extract data
+            df = pd.read_csv(url)
+            df = helper_df_cleaning(df)
+            concerned_columns = ['Lat', 'Long_']
+            df = helper_df_cols_cleaning(df, concerned_columns, float)
+            dataframes[category] = df
+
+        return dataframes
 
 
 # Get data from time series
@@ -75,26 +96,6 @@ def get_data_time_series() -> Dict[str, pd.DataFrame]:
         dataframes[category] = df
 
     return dataframes
-
-
-# Get data from time series (US)
-def get_US_time_series() -> Dict[str, pd.DataFrame]:
-    """ Get the dataset of time series for USA """
-    dataframes = {}
-
-    # Iterate through categories ('confirmed', 'deaths')
-    for category in JHU_CSSE_FILE_PATHS['CATEGORIES'][:-1]:
-        url = JHU_CSSE_FILE_PATHS['BASE_URL_US_TIME_SERIES'].format(category)
-        
-        # Extract data
-        df = pd.read_csv(url)
-        df = helper_df_cleaning(df)
-        concerned_columns = ['Lat', 'Long_']
-        df = helper_df_cols_cleaning(df, concerned_columns, float)
-        dataframes[category] = df
-
-    return dataframes
-
 
 # API v1
 def get_data(time_series: bool = False) -> Dict[str, pd.DataFrame]:
