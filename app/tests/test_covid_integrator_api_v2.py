e9e3bc863a7007408a35b1a83163e19678356f2d
Jerry Chen <chenhongjin2005@hotmail.com>
2021-03-17 03:13:11 -0400

    fixed and added tests

--- app/tests/test_covid_integrator_api_v2.py
+++ app/tests/test_covid_integrator_api_v2.py
@@ -9,8 +9,19 @@ import pytest
 import pandas as pd
 from ..utils import get_data
 from ..integrators import covid_api_v2_integrator
-
-
+from ..models.base_model import ResponseModel
+from ..models.covid_api_v2_model import (ActiveModel, ConfirmedModel,
+                                         CountryModel, CurrentModel,
+                                         CurrentUSModel, DeathsModel,
+                                         RecoveredModel,
+                                         TimeseriesCaseCoordinatesModel,
+                                         TimeseriesCaseDataModel,
+                                         TimeseriesCaseModel,
+                                         TimeseriesGlobalModel,
+                                         TimeseriesUSCoordinatesModel,
+                                         TimeseriesUSDataModel,
+                                         TimeseriesUSInfoModel,
+                                         TimeseriesUSModel, TotalModel)
 # Prepare the model
 INTEGRATOR = covid_api_v2_integrator.CovidAPIv2Integrator()
 
@@ -30,109 +41,109 @@ def test_init() -> None:
 
 # Test - Get Current
 def test_get_current() -> None:
-    result = INTEGRATOR.get_current()['data']
+    result = INTEGRATOR.get_current().data
     assert isinstance(result, list) is True
-    assert isinstance(result[0], dict) is True
-    assert isinstance(result[0]['location'], str) is True
-    assert isinstance(result[0]['confirmed'], int) is True
-    assert isinstance(result[0]['deaths'], int) is True
-    assert isinstance(result[0]['recovered'], int) is True
-    assert isinstance(result[0]['active'], int) is True
+    assert isinstance(result[0], CurrentModel) is True
+    assert isinstance(result[0].location, str) is True
+    assert isinstance(result[0].confirmed, int) is True
+    assert isinstance(result[0].deaths, int) is True
+    assert isinstance(result[0].recovered, int) is True
+    assert isinstance(result[0].active, int) is True
 
 
 # Test - Get country
 def test_get_country() -> None:
-    result = INTEGRATOR.get_country('th')['data']
-    assert isinstance(result, dict) is True
-    assert result['location'] == 'Thailand'
+    result = INTEGRATOR.get_country('th').data
+    assert isinstance(result, CountryModel) is True
+    assert result.location == 'Thailand'
 
-    result = INTEGRATOR.get_country('china')['data']
-    assert isinstance(result, dict) is True
-    assert result['location'] == 'China'
+    result = INTEGRATOR.get_country('china').data
+    assert isinstance(result, CountryModel) is True
+    assert result.location == 'China'
 
 
 # Test - Get Confirmed
 def test_get_confirmed() -> None:
     result = INTEGRATOR.get_confirmed()
-    assert isinstance(result, dict) is True
-    assert isinstance(result['data'], int) is True
+    assert isinstance(result, ResponseModel) is True
+    assert isinstance(result.data, ConfirmedModel) is True
 
 
 # Test - Get Deaths
 def test_get_deaths() -> None:
     result = INTEGRATOR.get_confirmed()
-    assert isinstance(result, dict) is True
-    assert isinstance(result['data'], int) is True
+    assert isinstance(result, ResponseModel) is True
+    assert isinstance(result.data, ConfirmedModel) is True
 
 
 # Test - Get Recovered
 def test_get_recovered() -> None:
     result = INTEGRATOR.get_confirmed()
-    assert isinstance(result, dict) is True
-    assert isinstance(result['data'], int) is True
+    assert isinstance(result, ResponseModel) is True
+    assert isinstance(result.data, ConfirmedModel) is True
 
 
 # Test - Get Active
 def test_get_active() -> None:
     result = INTEGRATOR.get_confirmed()
-    assert isinstance(result, dict) is True
-    assert isinstance(result['data'], int) is True
+    assert isinstance(result, ResponseModel) is True
+    assert isinstance(result.data, ConfirmedModel) is True
 
 
 # Test - Get Total
 def test_get_total() -> None:
-    result = INTEGRATOR.get_total()['data']
-    assert isinstance(result, dict) is True
-    assert isinstance(result['confirmed'], int) is True
-    assert isinstance(result['deaths'], int) is True
-    assert isinstance(result['recovered'], int) is True
-    assert isinstance(result['active'], int) is True
+    result = INTEGRATOR.get_total().data
+    assert isinstance(result, TotalModel) is True
+    assert isinstance(result.confirmed, int) is True
+    assert isinstance(result.deaths, int) is True
+    assert isinstance(result.recovered, int) is True
+    assert isinstance(result.active, int) is True
 
 
 # Test - Get Time Series
 def test_get_time_series() -> None:
-    result = INTEGRATOR.get_time_series('global')['data']
+    result = INTEGRATOR.get_time_series('global').data
     assert isinstance(result, list) is True
 
-    result = INTEGRATOR.get_time_series('confirmed')['data']
+    result = INTEGRATOR.get_time_series('confirmed').data
     assert isinstance(result, list) is True
-    assert isinstance(result[0]['Province/State'], str) is True
-    assert isinstance(result[0]['Country/Region'], str) is True
-    assert isinstance(result[0]['Coordinates'], dict) is True
-    assert isinstance(result[0]['TimeSeries'], list) is True
+    assert isinstance(result[0].Province_State, str) is True
+    assert isinstance(result[0].Country_Region, str) is True
+    assert isinstance(result[0].Coordinates, TimeseriesCaseCoordinatesModel) is True
+    assert isinstance(result[0].TimeSeries, list) is True
 
-    result = INTEGRATOR.get_time_series('deaths')['data']
+    result = INTEGRATOR.get_time_series('deaths').data
     assert isinstance(result, list) is True
-    assert isinstance(result[0]['Province/State'], str) is True
-    assert isinstance(result[0]['Country/Region'], str) is True
-    assert isinstance(result[0]['Coordinates'], dict) is True
-    assert isinstance(result[0]['TimeSeries'], list) is True
+    assert isinstance(result[0].Province_State, str) is True
+    assert isinstance(result[0].Country_Region, str) is True
+    assert isinstance(result[0].Coordinates, TimeseriesCaseCoordinatesModel) is True
+    assert isinstance(result[0].TimeSeries, list) is True
 
-    result = INTEGRATOR.get_time_series('recovered')['data']
+    result = INTEGRATOR.get_time_series('recovered').data
     assert isinstance(result, list) is True
-    assert isinstance(result[0]['Province/State'], str) is True
-    assert isinstance(result[0]['Country/Region'], str) is True
-    assert isinstance(result[0]['Coordinates'], dict) is True
-    assert isinstance(result[0]['TimeSeries'], list) is True
+    assert isinstance(result[0].Province_State, str) is True
+    assert isinstance(result[0].Country_Region, str) is True
+    assert isinstance(result[0].Coordinates, TimeseriesCaseCoordinatesModel) is True
+    assert isinstance(result[0].TimeSeries, list) is True
 
 
 # Test - Get Time Series (US)
 def test_get_US_time_series() -> None:
     result = INTEGRATOR.get_US_time_series('gibberish')
-    assert bool(result['data']) is False
+    assert bool(result.data) is False
 
-    result = INTEGRATOR.get_US_time_series('confirmed')['data']
+    result = INTEGRATOR.get_US_time_series('confirmed').data
     assert isinstance(result, list) is True
-    assert isinstance(result[0]['Province_State'], str) is True
-    assert isinstance(result[0]['Country_Region'], str) is True
-    assert isinstance(result[0]['Info'], dict) is True
-    assert isinstance(result[0]['Coordinates'], dict) is True
-    assert isinstance(result[0]['TimeSeries'], list) is True
+    assert isinstance(result[0].Province_State, str) is True
+    assert isinstance(result[0].Country_Region, str) is True
+    assert isinstance(result[0].Info, TimeseriesUSInfoModel) is True
+    assert isinstance(result[0].Coordinates, TimeseriesUSCoordinatesModel) is True
+    assert isinstance(result[0].TimeSeries, list) is True
 
-    result = INTEGRATOR.get_US_time_series('deaths')['data']
+    result = INTEGRATOR.get_US_time_series('deaths').data
     assert isinstance(result, list) is True
-    assert isinstance(result[0]['Province_State'], str) is True
-    assert isinstance(result[0]['Country_Region'], str) is True
-    assert isinstance(result[0]['Info'], dict) is True
-    assert isinstance(result[0]['Coordinates'], dict) is True
-    assert isinstance(result[0]['TimeSeries'], list) is True
+    assert isinstance(result[0].Province_State, str) is True
+    assert isinstance(result[0].Country_Region, str) is True
+    assert isinstance(result[0].Info, TimeseriesUSInfoModel) is True
+    assert isinstance(result[0].Coordinates, TimeseriesUSCoordinatesModel) is True
+    assert isinstance(result[0].TimeSeries, list) is True
--- app/tests/test_get_data.py
+++ app/tests/test_get_data.py
@@ -6,9 +6,12 @@ DATE: 11-April-2020
 """
 # Import libraries
 import pandas as pd
-from ..utils.get_data import DailyReports, get_data_lookup_table, get_data_time_series, get_US_time_series, get_data
+from ..utils.get_data import COVIDDataForAPIv1, COVIDDataForAPIv2, get_data_lookup_table
+from ..utils import file_paths 
 
-daily_reports = DailyReports()
+# Test DataFrame
+DATA = {'col_1': list(range(5)), 'col_2': [1, None, '', None, None]}
+DF = pd.DataFrame(DATA)
 
 # Test - Get Lookup table
 def test_get_data_lookup_table() -> None:
@@ -19,41 +22,73 @@ def test_get_data_lookup_table() -> None:
 
 # Test - Get data from daily reports
 def test_get_data_daily_reports() -> None:
-    result = daily_reports.get_data_daily_reports()
+    result = COVIDDataForAPIv2.get_data_daily_reports()
     assert len(result) > 0
     assert isinstance(result, pd.DataFrame) is True 
 
 
 # Test - Get data from daily reports (US)
 def test_get_data_daily_reports_US() -> None:
-    result = daily_reports.get_data_daily_reports(US=True)
+    result = COVIDDataForAPIv2.get_data_daily_reports(US=True)
     assert len(result) > 0
     assert isinstance(result, pd.DataFrame) is True 
 
 
 # Test - Get data from time series
 def test_get_data_time_series() -> None:
-    result = get_data_time_series()
+    result = COVIDDataForAPIv2.get_data_time_series()
     assert len(result) > 0
     assert isinstance(result, dict) is True
 
 
 # Test - Get data from time series (US)
 def test_get_US_time_series() -> None:
-    result = get_US_time_series()
+    result = COVIDDataForAPIv2.get_US_time_series()
     assert len(result) > 0
     assert isinstance(result, dict) is True
 
 
 # Test - Get Data (API v1)
 def test_get_data() -> None:
-    result = get_data()
+    result = COVIDDataForAPIv1.get_data()
     assert len(result) > 0
     assert isinstance(result, dict) is True
 
 
 # Test - Get Data (API v1)
 def test_get_data(time_series = True) -> None:
-    result = get_data()
+    result = COVIDDataForAPIv1.get_data()
     assert len(result) > 0
     assert isinstance(result, dict) is True
+
+
+# Test df cleaning
+def test_helper_df_cleaning() -> None:
+    df = DF.copy()
+    res_df = COVIDDataForAPIv2._df_cleaning(df)
+    res_col_1 = [i for i in res_df['col_1'].values]
+    res_col_2 = [i for i in res_df['col_2'].values]
+    assert isinstance(res_df, pd.DataFrame) is True
+    assert res_col_1 == [i for i in DF['col_1'].values]
+    assert res_col_2 == [1, '', '', '', '']
+
+
+# Test df cleaning on columns
+def test_helper_df_cols_cleaning() -> None:
+    df = DF.copy()
+    res_df = COVIDDataForAPIv2._df_cols_cleaning(df, ['col_2'], str)
+    res_col_1 = [i for i in res_df['col_1'].values]
+    res_col_2 = [i for i in res_df['col_2'].values]
+    assert isinstance(res_df, pd.DataFrame) is True
+    assert res_col_1 == [i for i in DF['col_1'].values]
+    assert res_col_2 != [i for i in DF['col_2'].values]
+
+
+# Test get latest data
+def test_helper_get_latest_data_url() -> None:
+    assert isinstance(COVIDDataForAPIv2._get_latest_data_url(file_paths.JHU_CSSE_FILE_PATHS['BASE_URL_LOOKUP_TABLE']), str)
+    assert isinstance(COVIDDataForAPIv2._get_latest_data_url(file_paths.JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS']), str)
+    assert isinstance(COVIDDataForAPIv2._get_latest_data_url(file_paths.JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS_US']), str)
+    ## Temporily disable timeseries testing
+    # assert isinstance(COVIDDataForAPIv2._get_latest_data_url(file_paths.JHU_CSSE_FILE_PATHS['BASE_URL_TIME_SERIES']), str)
+    # assert isinstance(COVIDDataForAPIv2._get_latest_data_url(file_paths.JHU_CSSE_FILE_PATHS['BASE_URL_US_TIME_SERIES']), str)
