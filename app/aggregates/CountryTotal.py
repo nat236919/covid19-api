8f7166ae71a1d371179c8ad54d40923e76fab673
uaabduls <vivian.a.dcruze@gmail.com>
2021-03-17 20:57:56 -0400

    implemented aggregate to create new CountryTotal model, added model v3

---
+++ app/aggregates/CountryTotal.py
@@ -0,0 +1,18 @@
+"""
+FILE: CountryTotal.py
+DESCRIPTION: Encapsulation of all Time related data for a query
+AUTHOR: Vivian Dcruze
+DATE: 15-March-2021
+"""
+
+from models.covid_api_v1_model import (CountriesModel, TotalModel)
+
+class CountryTotal:
+    totalmodel: TotalModel
+    countrymodel: CountriesModel
+    country: str
+
+    def __init__(self, tm: TotalModel, cm: CountriesModel, country: str) -> None:
+        self.totalmodel = date
+        self.countrymodel = timestamp
+        self.country = country
---
+++ app/models/covid_api_v3_model.py
@@ -0,0 +1,18 @@
+"""
+FILE: covid_api_v2_model.py
+DESCRIPTION: Models for API v3
+AUTHOR: Nuttaphat Arunoprayoch
+DATE: 15-March-2021
+"""
+# Import libraries
+from typing import List
+
+from pydantic import BaseModel
+
+from aggregates import CountryTotal
+
+#######################################
+# CountryTotalModel
+#######################################
+class CountryTotalModel
+    countryTotal: CountryTotalModel
