e21a0c15eb39dba1bcd2b90a3600bae2ee74d4e7
sparsh210 <77583854+sparsh210@users.noreply.github.com>
2021-03-26 20:30:02 -0400

    Create config.py
    Here I fixed the spacing as well

--- app/config.py
+++ app/config.py
@@ -4,9 +4,8 @@ DESCRIPTION: Configurations
 AUTHOR: Nuttaphat Arunoprayoch
 DATE: 26-Nov-2020
 """
-
 CONFIG = {
-    'app': {
+    'app':  {
         'title': 'COVID-19 API',
         'description': 'Simply FAST API for COVID-19 cases exploration',
         'version': '2.1.0'
