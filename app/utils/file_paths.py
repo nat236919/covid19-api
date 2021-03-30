e230a36e32bdb7c9aa2baa9fa33f571844c7b545
Prince Patel <prince.patelcad@gmail.com>
2021-03-30 07:16:58 -0400

    Fixed the error in reformatted 'file_paths.py' implementation [2]

--- app/utils/file_paths.py
+++ app/utils/file_paths.py
@@ -181,20 +181,3 @@ JHU_CSSE_FILE_PATHS = {
     'BASE_URL_US_TIME_SERIES': Director.construct_US_TimeSeries_Path(),
     'CATEGORIES': JHU_CSSE_CATEGORIES
 }
-
-# JHU_CSSE_BASE_PATH = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/'
-# JHU_CSSE_DAILY_REPORT_PATH = JHU_CSSE_BASE_PATH + 'csse_covid_19_daily_reports/'
-# JHU_CSSE_DAILY_REPORT_US_PATH = JHU_CSSE_BASE_PATH + \
-#     'csse_covid_19_daily_reports_us/'
-# JHU_CSSE_TIME_SERIES_PATH = JHU_CSSE_BASE_PATH + 'csse_covid_19_time_series/'
-# JHU_CSSE_LOOKUP_TABLE_PATH = JHU_CSSE_BASE_PATH + 'UID_ISO_FIPS_LookUp_Table.csv'
-# JHU_CSSE_CATEGORIES = ['confirmed', 'deaths', 'recovered']
-
-# JHU_CSSE_FILE_PATHS = {
-#     'BASE_URL_LOOKUP_TABLE': JHU_CSSE_LOOKUP_TABLE_PATH,
-#     'BASE_URL_DAILY_REPORTS': JHU_CSSE_DAILY_REPORT_PATH + '{}.csv',
-#     'BASE_URL_DAILY_REPORTS_US': JHU_CSSE_DAILY_REPORT_US_PATH + '{}.csv',
-#     'BASE_URL_TIME_SERIES': JHU_CSSE_TIME_SERIES_PATH + 'time_series_covid19_{}_global.csv',
-#     'BASE_URL_US_TIME_SERIES': JHU_CSSE_TIME_SERIES_PATH + 'time_series_covid19_{}_US.csv',
-#     'CATEGORIES': JHU_CSSE_CATEGORIES
-# }
