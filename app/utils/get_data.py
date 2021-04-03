218398e8ebed9c5e255eaeea4b612f55045f985c
sparsh210 <77583854+sparsh210@users.noreply.github.com>
2021-04-03 13:25:14 -0400

    Making DataProvider class for get_data file
    It is correct that many data providers for this app is John Hopkins University but, we should create the app such that it is able to get data from different sources in case that later there are more sources of data for covid cases. To do this, we should make a data provider class and receives that data source name and URLs and applies different get data functions based on these inputs.

--- app/utils/get_data.py
+++ app/utils/get_data.py
@@ -14,118 +14,138 @@ from .file_paths import JHU_CSSE_FILE_PATHS
 from .helper import (helper_df_cleaning, helper_df_cols_cleaning,
                      helper_get_latest_data_url)
 
-
+class DataProvider:
+    def __init__(self, name, URL, baseURL, dailyURL, USdailyURL):
+        self.name = name
+        self.URL = URL
+        self.baseURL = baseURL
+        self.dailyURL = dailyURL
+        self.USdailyURL = USdailyURL
+          
 # Get Lookup table
-def get_data_lookup_table() -> Dict[str, str]:
-    """ Get lookup table (country references for iso2) """
-    lookup_table_url = JHU_CSSE_FILE_PATHS['BASE_URL_LOOKUP_TABLE']
-    lookup_df = pd.read_csv(lookup_table_url)[['iso2', 'Country_Region']]
-    
-    # Create referral dictionary
-    data = lookup_df.to_dict('records')
-    data = {v['iso2']: v['Country_Region'] for v in data}
+    def get_data_lookup_table() -> Dict[str, str]:
+        """ Get lookup table (country references for iso2) """
+        #lookup_table_url = JHU_CSSE_FILE_PATHS['BASE_URL_LOOKUP_TABLE']
+        #lookup_df = pd.read_csv(lookup_table_url)[['iso2', 'Country_Region']]
+        
+        URL = name['BASE_URL_LOOKUP_TABLE']
+        lookup_df = pd.read_csv(URL)[['iso2', 'Country_Region']]
+        
+        # Create referral dictionary
+        data = lookup_df.to_dict('records')
+        data = {v['iso2']: v['Country_Region'] for v in data}
 
-    return data
+        return data
 
 
 # Get data from daily reports
-def get_data_daily_reports() -> pd.DataFrame:
-    """ Get data from BASE_URL_DAILY_REPORTS """
-    # Check the latest file
-    latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS'])
+    def get_data_daily_reports() -> pd.DataFrame:
+        """ Get data from BASE_URL_DAILY_REPORTS """
+        # Check the latest file
+        #latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS'])
 
-    # Extract the data
-    df = pd.read_csv(latest_base_url)
+        baseURL = helper_get_latest_data_url(name['BASE_URL_DAILY_REPORTS'])
+        # Extract the data
+        #df = pd.read_csv(latest_base_url)
+        df = pd.read_csv(baseURL) 
 
-    # Data pre-processing
-    concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active']
-    df = helper_df_cols_cleaning(df, concerned_columns, int)
-    
-    return df
+        # Data pre-processing
+        concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active']
+        df = helper_df_cols_cleaning(df, concerned_columns, int)
+        
+        return df
 
 
 # Get data from daily reports (USA)
-def get_data_daily_reports_us() -> pd.DataFrame:
-    """ Get data from BASE_URL_DAILY_REPORTS """
-    # Check the latest file
-    latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS_US'])
+    def get_data_daily_reports_us() -> pd.DataFrame:
+        """ Get data from BASE_URL_DAILY_REPORTS """
+        # Check the latest file
+        #latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS_US'])
+        USdailyURL = helper_get_latest_data_url(name['BASE_URL_DAILY_REPORTS_US'])
 
-    # Extract the data
-    df = pd.read_csv(latest_base_url)
+        # Extract the data
+        #df = pd.read_csv(latest_base_url)
+        df = pd.read_csv(USdailyURL)        
 
-    # Data pre-processing
-    concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active']
-    df = helper_df_cols_cleaning(df, concerned_columns, int)
-    
-    return df
+        # Data pre-processing
+        concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active', 'People_Tested',
+                            'People_Hospitalized']
+        df = helper_df_cols_cleaning(df, concerned_columns, int)
+        
+        return df
 
 
 # Get data from time series
-def get_data_time_series() -> Dict[str, pd.DataFrame]:
-    """ Get the dataset from JHU CSSE """
-    dataframes = {}
+    def get_data_time_series() -> Dict[str, pd.DataFrame]:
+        """ Get the dataset from JHU CSSE """
+        dataframes = {}
 
-    # Iterate through all files
-    for category in JHU_CSSE_FILE_PATHS['CATEGORIES']:
-        url = JHU_CSSE_FILE_PATHS['BASE_URL_TIME_SERIES'].format(category)
+        # Iterate through all files
+        for category in JHU_CSSE_FILE_PATHS['CATEGORIES']:
+            #url = JHU_CSSE_FILE_PATHS['BASE_URL_TIME_SERIES'].format(category)
+            url = name['BASE_URL_TIME_SERIES'].format(category)
 
-        # Extract data
-        df = pd.read_csv(url)
-        df = helper_df_cleaning(df)
-        dataframes[category] = df
+            # Extract data
+            df = pd.read_csv(url)
+            df = helper_df_cleaning(df)
+            dataframes[category] = df
 
-    return dataframes
-
-
-# Get data from time series (US)
-def get_US_time_series() -> Dict[str, pd.DataFrame]:
-    """ Get the dataset of time series for USA """
-    dataframes = {}
-
-    # Iterate through categories ('confirmed', 'deaths')
-    for category in JHU_CSSE_FILE_PATHS['CATEGORIES'][:-1]:
-        url = JHU_CSSE_FILE_PATHS['BASE_URL_US_TIME_SERIES'].format(category)
-        
-        # Extract data
-        df = pd.read_csv(url)
-        df = helper_df_cleaning(df)
-        concerned_columns = ['Lat', 'Long_']
-        df = helper_df_cols_cleaning(df, concerned_columns, float)
-        dataframes[category] = df
-
-    return dataframes
+        return dataframes
 
 
-# API v1
-def get_data(time_series: bool = False) -> Dict[str, pd.DataFrame]:
-    """ Get the dataset from JHU CSSE """
-    dataframes = {}
+    # Get data from time series (US)
+    def get_US_time_series() -> Dict[str, pd.DataFrame]:
+        """ Get the dataset of time series for USA """
+        dataframes = {}
 
-    # Iterate through all files
-    for category in JHU_CSSE_FILE_PATHS['CATEGORIES']:
-        url = JHU_CSSE_FILE_PATHS['BASE_URL_TIME_SERIES'].format(category)
+        # Iterate through categories ('confirmed', 'deaths')
+        for category in JHU_CSSE_FILE_PATHS['CATEGORIES'][:-1]:
+            #url = JHU_CSSE_FILE_PATHS['BASE_URL_US_TIME_SERIES'].format(category)
+            url = name['BASE_URL_US_TIME_SERIES'].format(category)
+            
+            # Extract data
+            df = pd.read_csv(url)
+            df = helper_df_cleaning(df)
+            concerned_columns = ['Lat', 'Long_']
+            df = helper_df_cols_cleaning(df, concerned_columns, float)
+            dataframes[category] = df
 
-        # Extract data
-        df = pd.read_csv(url)
-        df = df.fillna('')
-        df['Country/Region'] = df['Country/Region'].apply(lambda country_name: country_name.strip()) # Eliminate whitespace
-        df['Country/Region'] = df['Country/Region'].str.replace(' ', '_')
+        return dataframes
 
-        # Data Preprocessing
-        if time_series:
-            df = df.T.to_dict()
-        else:
-            df = df.iloc[:, [0, 1, -1]] # Select only Region, Country and its last values
-            datetime_raw = list(df.columns.values)[-1] # Ex) '2/11/20 20:44'
-            df.columns = ['Province/State', 'Country/Region', category]
 
-            df[category].fillna(0, inplace=True) # Replace empty cells with 0
-            df[category].replace('', 0, inplace=True) # Replace '' with 0
+    # API v1
+    def get_data(time_series: bool = False) -> Dict[str, pd.DataFrame]:
+        """ Get the dataset from JHU CSSE """
+        dataframes = {}
 
-            df['datetime'] = datetime_raw
-            pd.to_numeric(df[category])
-            df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+        # Iterate through all files
+        for category in JHU_CSSE_FILE_PATHS['CATEGORIES']:
+            #url = JHU_CSSE_FILE_PATHS['BASE_URL_TIME_SERIES'].format(category)
+            url = name['BASE_URL_TIME_SERIES'].format(category)
 
-        dataframes[category.lower()] = df
+            # Extract data
+            df = pd.read_csv(url)
+            df = df.fillna('')
+            df['Country/Region'] = df['Country/Region'].apply(lambda country_name: country_name.strip()) # Eliminate whitespace
+            df['Country/Region'] = df['Country/Region'].str.replace(' ', '_')
 
-    return dataframes
+            # Data Preprocessing
+            if time_series:
+                df = df.T.to_dict()
+            else:
+                df = df.iloc[:, [0, 1, -1]] # Select only Region, Country and its last values
+                datetime_raw = list(df.columns.values)[-1] # Ex) '2/11/20 20:44'
+                df.columns = ['Province/State', 'Country/Region', category]
+
+                df[category].fillna(0, inplace=True) # Replace empty cells with 0
+                df[category].replace('', 0, inplace=True) # Replace '' with 0
+
+                df['datetime'] = datetime_raw
+                pd.to_numeric(df[category])
+                df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+
+            dataframes[category.lower()] = df
+
+        return dataframes
+
+
