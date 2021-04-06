36d89470ac58e01dc628e5428513eb52094c7391
Ruchesh Kapadia <rucheshkapadia@gmail.com>
2021-04-06 17:39:24 -0400

    Update helper_clean_interface.py

--- app/utils/helper_clean_interface.py
+++ app/utils/helper_clean_interface.py
@@ -8,13 +8,13 @@ import requests
 
 class IHelperClean(metaclass=ABCMeta):
 
-@staticmethod
-@abstractmethod
-def helper_df_cleaning(df: pd.DataFrame) -> pd.DataFrame:
-    """ Fill all empty values with am empty string (e.g., '') """
+    @staticmethod
+    @abstractmethod
+    def helper_df_cleaning(df: pd.DataFrame) -> pd.DataFrame:
+        """ Fill all empty values with am empty string (e.g., '') """
 
 
-@staticmethod
-@abstractmethod
-def helper_df_cols_cleaning(df: pd.DataFrame, cols: List[str], ensure_dtype: var_types = None) -> pd.DataFrame:
-    """ Clean certain colomns in a DataFrame """
+    @staticmethod
+    @abstractmethod
+    def helper_df_cols_cleaning(df: pd.DataFrame, cols: List[str], ensure_dtype: var_types = None) -> pd.DataFrame:
+        """ Clean certain colomns in a DataFrame """
