23adb3c600f94da84a850c06400a667a7361bbc5
Ruchesh Kapadia <rucheshkapadia@gmail.com>
2021-04-06 17:38:31 -0400

    Update helper.py

--- app/utils/helper.py
+++ app/utils/helper.py
@@ -12,26 +12,6 @@ import pandas as pd
 import pycountry
 import requests
 
-
-# Data preprocessing (DataFrame)
-def helper_df_cleaning(df: pd.DataFrame) -> pd.DataFrame:
-    """ Fill all empty values with am empty string (e.g., '') """
-    return df.fillna('')
-
-
-# Data preprocessing (DataFrame's columns)
-var_types = TypeVar('ensure_dtype', int, float, str)
-def helper_df_cols_cleaning(df: pd.DataFrame, cols: List[str], ensure_dtype: var_types = None) -> pd.DataFrame:
-    """ Clean certain colomns in a DataFrame """
-    df[cols] = df[cols].fillna(0) # Replace empty cells with 0
-    df[cols] = df[cols].replace('', 0) # Replace '' with 0
-
-    if ensure_dtype and ensure_dtype in [int, float, str]:
-        df[cols] = df[cols].astype(ensure_dtype)
-
-    return df
-
-
 # Get latest data
 def helper_get_latest_data_url(base_url: str) -> str:
     """ Get the latest base URL """
