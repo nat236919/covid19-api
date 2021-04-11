e5a85120f5cffa1f37b4dfc9ca3304d89216b588
Ruchesh Kapadia <rucheshkapadia@gmail.com>
2021-04-11 19:48:12 -0400

    Update daily_reports_adapter.py

--- app/utils/daily_reports_adapter.py
+++ app/utils/daily_reports_adapter.py
@@ -7,8 +7,8 @@ import pandas as pd
 from .file_paths import JHU_CSSE_FILE_PATHS
 from .helper import (helper_df_cleaning, helper_df_cols_cleaning,
                      helper_get_latest_data_url)
-from time_series_interface import IDataTimeSeries
-from daily_report import DailyReports
+from .time_series_interface import IDataTimeSeries
+from .daily_report import DailyReports
 
 class DailyReportsAdapter(IDataTimeSeries)
     def __init__(self):
