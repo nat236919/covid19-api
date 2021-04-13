c86550763dc915010315443d89f10123919780d8
Kevvol <helsinkiwhite@gmail.com>
2021-04-12 21:47:01 -0400

    added *args to cahce and adapter for the cache object, minor changes to v2_integrator to use new methods with same names

--- app/utils/caching.py
+++ app/utils/caching.py
@@ -41,6 +41,7 @@ class COVID_APICache:
         "data_daily_reports_us": get_data_daily_reports_us,
         "data_time_series": get_data_time_series,
         "US_time_series": get_US_time_series,
+        "data": get_data,
         }
         self.expire_time_dict = {}
         self.expire_time = expire_time
@@ -49,14 +50,14 @@ class COVID_APICache:
     the actual public method to get the cached values
     returns cached data if there is active cahced data
     """
-    def get(self, name):
+    def get(self, name, *args):
         # if there is an active cache and the expiration time has not been reached
         if name in self.cached_values and name in self.expire_time_dict and datetime.now() < self.expire_time_dict[name]:
             # return the cached data
             return self.cached_values[name]
         else: # if there is a need to fetch new data
             # if the cache has not be populated at all or expire time reached
-            self.__get_value_from_server(name)
+            self.__get_value_from_server(name, *args)
             # reset expiration time
             self.expire_time_dict[name] = datetime.now() + timedelta(minutes=self.expire_time)
             return self.cached_values[name]
@@ -65,8 +66,8 @@ class COVID_APICache:
     using code from get_data.py
     fetchs a value from the JHU server and store it in the cache
     """
-    def __get_value_from_server(self, name):
-        self.cached_values[name] = self.fetch_methods[name]()
+    def __get_value_from_server(self, name, *args):
+        self.cached_values[name] = self.fetch_methods[name](*args)
 
     """
     setter method to change the expire time
@@ -77,3 +78,30 @@ class COVID_APICache:
 # create the instant of the object for all other code to access
 # there can be multiple cache objects of course
 api_cache = COVID_APICache()
+
+# an adapter for the COVID_APICache object
+# with the cache object (or rather a reference to it) inside as the adaptee
+# so that the same method names can be used in any client using the get data code parts with the cache
+# to minimize confusion, complexity, and the need to change code
+class API_cache_adapter(object):
+    cache: COVID_APICache
+    def __init__(self, c):
+        self.cache = c
+
+    def get_data_lookup_table(self) -> Dict[str, str]:
+        return self.cache.get("lookup_table")
+
+    def get_data_daily_reports(self) -> pd.DataFrame:
+        return self.cache.get("data_daily_reports")
+
+    def get_data_daily_reports_us(self) -> pd.DataFrame:
+        return self.cache.get("data_daily_reports_us")
+
+    def get_data_time_series(self) -> Dict[str, pd.DataFrame]:
+        return self.cache.get("data_time_series")
+
+    def get_US_time_series(self) -> Dict[str, pd.DataFrame]:
+        return self.cache.get("US_time_series")
+
+    def get_data(time_series: bool = False) -> Dict[str, pd.DataFrame]:
+        return self.cache.get("data", time_series)
