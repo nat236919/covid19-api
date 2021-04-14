0f5883408e0cfe6b249d36eac3cf6fae8c448b61
Aman Patel <amanpatel9824@gmail.com>
2021-04-14 16:37:04 -0400

    Add bridge pattern for Vaccine

--- app/utils/vaccine.py
+++ app/utils/vaccine.py
@@ -4,26 +4,34 @@ from typing import Any
 import requests
 
 
-class vaccine:
+class vaccine_abstraction:
+
+    def __init__(self, imp):
+        self._imp = imp
+
+    def get_vaccine_data(self) -> Any:
+        return self._imp.get_vaccine_data()
+
+
+class vaccine_implementor:
     """ Model and Its methods """
     __instance = None
 
     @staticmethod
     def getInstance():
-        if vaccine.__instance is None:
+        if vaccine_implementor.__instance is None:
             """creating single instance """
-            vaccine()
-            return vaccine.__instance
+            vaccine_implementor()
+            return vaccine_implementor.__instance
 
     def __init__(self):
         self.url = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/vaccines/get-all-treatment"
-
         self.headers = {
             'x-rapidapi-key': "770aeab636msh7d96b4b6f420dc6p19b93ejsnefbca91bf4fa",
             'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
         }
 
-    def get_vaccine_data(self) -> Any:
+    def get_vaccine_data(self):
         try:
             response = requests.request("GET", self.url, headers=self.headers)
             # Get the list of the json object, but since the api only get the single object at a time, can get it
@@ -31,3 +39,17 @@ class vaccine:
             return response.json()
         except Exception as e:
             return e
+
+
+class vaccine:
+    """
+    vaccine_implementor : Concrete Implementor
+    vaccine_abstraction : Abstraction's Interface
+    """
+
+    def __init__(self):
+        self.v = vaccine_implementor()
+        self.v_abs = vaccine_abstraction(self.v)
+
+    def get_vaccine_data(self):
+        return self.v_abs.get_vaccine_data()
