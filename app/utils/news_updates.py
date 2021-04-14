d1caed036d04e2d24b57ad1d993dc4c143e5664b
Roshan <roshan22@my.yorku.ca>
2021-04-15 02:35:21 +0530

    Adapter Structural news updates

--- app/utils/news_updates.py
+++ app/utils/news_updates.py
@@ -3,16 +3,32 @@ from typing import Any
 import requests
 
 
-class news_updates:
+class Target:
+
+    def __init__(self):
+        self._news = NewsUpdatesData()
+
+    # Abstract Method
+    def get_request(self):
+        pass
+
+
+class NewsAdapter(Target):
+
+    def get_request(self):
+        return self._news.get_news_updates_india()
+
+
+class NewsUpdatesData:
     """ Model and Its methods """
     instance = None
 
     @staticmethod
     def getInstance():
-        if news_updates.instance is None:
+        if NewsUpdatesData.instance is None:
             """here we are creating singleton instance """
-            news_updates()
-            return news_updates.instance
+            NewsUpdatesData()
+            return NewsUpdatesData.instance
 
     def __init__(self):
         self.url = "https://covid-19-india2.p.rapidapi.com/latest.php"
@@ -29,5 +45,10 @@ class news_updates:
             return E
 
 
-i = news_updates()
-print(i.get_news_updates_india())
+class news_updates:
+
+    def __init__(self):
+        self.newsAdapter = NewsAdapter()
+
+    def get_news_updates_india(self):
+        return self.newsAdapter.get_request()
