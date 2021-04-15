19dbc0b734f69b698a3de7c147b474d2f3aedc72
oTitanium <dervisyasin_50@hotmail.com>
2021-04-15 17:49:49 -0400

    Add a Structural Pattern
    Add a Structural Pattern

--- app/utils/get_data.py
+++ app/utils/get_data.py
@@ -1,114 +1,64 @@
-"""
-FILE: get_data.py
-DESCRIPTION: Read raw files from GitHub
-AUTHOR: Nuttaphat Arunoprayoch
-DATE: 9-Feb-2020
-"""
-# Import libraries
-import csv
-from typing import Dict
-
 import pandas as pd
-
-from .file_paths import JHU_CSSE_FILE_PATHS
-from .helper import (helper_df_cleaning, helper_df_cols_cleaning,
-                     helper_get_latest_data_url)
-
-
-# Get Lookup table
-def get_data_lookup_table() -> Dict[str, str]:
-    """ Get lookup table (country references for iso2) """
-    lookup_table_url = JHU_CSSE_FILE_PATHS['BASE_URL_LOOKUP_TABLE']
-    lookup_df = pd.read_csv(lookup_table_url)[['iso2', 'Country_Region']]
-    
-    # Create referral dictionary
-    data = lookup_df.to_dict('records')
-    data = {v['iso2']: v['Country_Region'] for v in data}
-
-    return data
+from urllib import request
+from os import getcwd, path
+import datetime
+from helpers import ensure_dirs
 
 
-# Get Daily Reports Data (General and US)
-class DailyReports:
-    def __init__(self) -> None: 
-        self.latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS'])
-        self.latest_base_US_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS_US'])
+COUNTIES_DATASET = 'https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv'
+STATES_DATASET = 'https://github.com/nytimes/covid-19-data/raw/master/us-states.csv'
 
-    # Get data from daily reports
-    def get_data_daily_reports(self, US: bool = False) -> pd.DataFrame:
-        """ Get data from BASE_URL_DAILY_REPORTS """
-        # Extract the data
-        df = pd.read_csv(self.latest_base_US_url) if US else pd.read_csv(self.latest_base_url)
 
-        # Data pre-processing
-        concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active']
-        df = helper_df_cols_cleaning(df, concerned_columns, int)
-        
-        return df
+def scrape_united_states_of_america():
+    cwd = getcwd()
+    us_dir = path.join(cwd, 'data', 'united_states_of_america')
+    tmp_dir = path.join(cwd, 'tmp')
 
-      
-# Get data from time series (General and US)
-class DataTimeSeries:
-    """ Get the tiemseires dataset from JHU CSSE and Prepare DataFrames """
-    def get_data_time_series(self, US: bool = False) -> Dict[str, pd.DataFrame]:
-        """ Get the dataset from JHU CSSE """
-        dataframes = {}
+    ensure_dirs(us_dir, tmp_dir)
 
-        # Determine categories and url
-        if US:
-            categories = JHU_CSSE_FILE_PATHS['CATEGORIES'][:-1] # Select only 'confirmed' and 'deaths'
-            url = JHU_CSSE_FILE_PATHS['BASE_URL_US_TIME_SERIES']
-        else:
-            categories = JHU_CSSE_FILE_PATHS['CATEGORIES']
-            url = JHU_CSSE_FILE_PATHS['BASE_URL_TIME_SERIES']
+    headers = ['date', 'state', 'county',
+               'place_type', 'fips', 'cases', 'deaths']
 
-        # Iterate through all files
-        for category in categories:
-            url = url.format(category)
-            # Extract data from URL
-            df = pd.read_csv(url)
-            df = self._clean_timeseries_dataframe(df, US)
-            dataframes[category] = df
+    counties_df = pd.read_csv(COUNTIES_DATASET)
+    counties_df = counties_df.sort_values(
+        by=['state', 'county', 'date'], ascending=[True, True, False])
+    counties_df['place_type'] = 'county'
+    counties_df = counties_df[headers]
 
-        return dataframes
-    
-    def _clean_timeseries_dataframe(self, df: pd.DataFrame, US: bool = False) -> pd.DataFrame:
-        df_cleaned = helper_df_cleaning(df) # main pre-processing
-        if US:
-            df_cleaned = helper_df_cols_cleaning(df_cleaned, ['Lat', 'Long_'], float)
-        return df_cleaned
+    states_df = pd.read_csv(STATES_DATASET)
+    states_df = states_df.sort_values(
+        by=['state', 'date'], ascending=[True, False])
+    states_df['county'] = ''
+    states_df['place_type'] = 'state'
+    states_df = states_df[headers]
 
-      
-# API v1
-def get_data(time_series: bool = False) -> Dict[str, pd.DataFrame]:
-    """ Get the dataset from JHU CSSE """
-    dataframes = {}
+    states_fips = {}
+    fipses = states_df['fips'].unique()
+    for fips in fipses:
+        is_current_fips = states_df['fips'] == fips
+        fips_file = path.join(us_dir, f'{fips:02d}.csv')
+        current_df = states_df[is_current_fips]
+        current_df.to_csv(fips_file, index=False, float_format='%.f')
 
-    # Iterate through all files
-    for category in JHU_CSSE_FILE_PATHS['CATEGORIES']:
-        url = JHU_CSSE_FILE_PATHS['BASE_URL_TIME_SERIES'].format(category)
+        state = current_df['state'].iloc[0]
+        is_same_fips = counties_df['state'] == state
+        current_counties_df = counties_df[is_same_fips]
+        current_counties_df.to_csv(
+            fips_file, index=False, header=False, mode='a', float_format='%.f')
 
-        # Extract data
-        df = pd.read_csv(url)
-        df = df.fillna('')
-        df['Country/Region'] = df['Country/Region'].apply(lambda country_name: country_name.strip()) # Eliminate whitespace
-        df['Country/Region'] = df['Country/Region'].str.replace(' ', '_')
+        states_fips[f'{fips:02d}'] = state
 
-        # Data Preprocessing
-        if time_series:
-            df = df.T.to_dict()
-        else:
-            df = df.iloc[:, [0, 1, -1]] # Select only Region, Country and its last values
-            datetime_raw = list(df.columns.values)[-1] # Ex) '2/11/20 20:44'
-            df.columns = ['Province/State', 'Country/Region', category]
+    with open(path.join(us_dir, 'README.md'), 'w') as readme_f:
+        readme_f.write(get_readme_contents(states_fips))
 
-            df[category].fillna(0, inplace=True) # Replace empty cells with 0
-            df[category].replace('', 0, inplace=True) # Replace '' with 0
 
-            df['datetime'] = datetime_raw
-            pd.to_numeric(df[category])
-            df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
-
-        dataframes[category.lower()] = df
-
-    return dataframes
+def get_readme_contents(states):
+    toc = [
+        f'| {state} | [`{fips}.csv`]({fips}.csv) |' for fips, state in states.items()]
+    toc_contents = '\n'.join(toc)
+    return f"""## United States of America
+> Last updated at {datetime.datetime.now(datetime.timezone.utc).strftime('%b %d %Y %H:%M:%S UTC')}.
+| State | Dataset |
+| ------ | ------- |
+{toc_contents}
+"""
