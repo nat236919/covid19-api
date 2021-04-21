060d2cef1bb692692bab43bb90ebe78c1360f146
harism91 <53982381+harism91@users.noreply.github.com>
2021-04-21 17:49:49 -0400

    state design pattern implementation

--- app/models/covid_api_v1_model.py
+++ app/models/covid_api_v1_model.py
@@ -9,6 +9,65 @@ from typing import List
 
 from pydantic import BaseModel
 
+# class model:
+#     """
+#     The Context defines the interface of interest to clients. It also maintains
+#     a reference to an instance of a State subclass, which represents the current
+#     state of the Context.
+#     """
+#
+#     _state = None
+#     """
+#     A reference to the current state of the Context.
+#     """
+#
+#     def __init__(self, state: State) -> None:
+#         self.transition_to(state)
+#
+#     def transition_to(self, state: State):
+#         """
+#         The Context allows changing the State object at runtime.
+#         """
+#
+#         print(f"Context: Transition to {type(state).__name__}")
+#         self._state = state
+#         self._state.context = self
+#
+#     """
+#     The Context delegates part of its behavior to the current State object.
+#     """
+#
+#     def request1(self):
+#         self._state.handle1()
+#
+#     def request2(self):
+#         self._state.handle2()
+#
+#
+# class State(baseModel):
+#     """
+#     The base State class declares methods that all Concrete State should
+#     implement and also provides a backreference to the Context object,
+#     associated with the State. This backreference can be used by States to
+#     transition the Context to another State.
+#     """
+#
+#     @property
+#     def context(self) -> Context:
+#         return self._context
+#
+#     @context.setter
+#     def context(self, context: Context) -> None:
+#         self._context = context
+#
+#     @abstractmethod
+#     def handle1(self) -> None:
+#         pass
+#
+#     @abstractmethod
+#     def handle2(self) -> None:
+#         pass
+#
 
 #######################################
 # CurrentModel
